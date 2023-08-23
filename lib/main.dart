7f412d626a3cd5d4640bf16b59ef648ae4c314a5
abdelbasset-mg <a_meghraoui@estin.dz>
2023-08-23 10:47:37 +0100

    start

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:expenses_tracker/expenses.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home : Expenses()
+    ),
+  );
+}
---
+++ lib/models/expense.dart
@@ -0,0 +1,19 @@
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+class Expense {
+
+Expense({
+  required this.title,
+  required this.amount,
+  required this.date,
+  }) : id = uuid.v4() ;
+
+final String title ;
+final double amount ;
+final DateTime date ;
+final String id ;
+
+
+}
