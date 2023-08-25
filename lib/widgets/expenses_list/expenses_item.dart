f1ebef705d24665247b486434754f1701f9fb1bd
abdelbasset-mg <a_meghraoui@estin.dz>
2023-08-25 22:11:17 +0100

    start expenses list

---
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -0,0 +1,14 @@
+import 'package:expenses_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+
+  const ExpensesList (this.expenses,{super.key,});
+
+  final Expense expenses;
+
+  @override
+  Widget build(context) {
+    return
+  }
+}
