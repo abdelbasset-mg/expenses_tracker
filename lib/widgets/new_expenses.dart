fc021ae33cca8832b5801103866ce7dda94c55e5
abdelbasset-mg <a_meghraoui@estin.dz>
2023-09-01 14:32:42 +0100

    new expense

--- lib/widgets/new_expenses.dart
+++ lib/widgets/new_expenses.dart
@@ -9,17 +9,51 @@ class NewExpenses extends StatefulWidget {
 }
 
 class _NewExpensesState extends State<NewExpenses> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
-            decoration : InputDecoration(
+            decoration: const InputDecoration(
               labelText: 'Title',
             ),
+          ),
+          TextField(
+            decoration: const InputDecoration(
+              prefixText: '\$ ',
+              labelText: 'Amount',
+            ),
+            keyboardType: TextInputType.number,
+            controller: _amountController,
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountController.text);
+                },
+                child: const Text('Add Expense'),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text('Cancel'),
+              ),
+            ],
           )
         ],
       ),
