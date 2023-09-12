39709c43d9805c3cb309038d0587668792666f1f
abdelbasset-mg <a_meghraoui@estin.dz>
2023-09-12 03:38:27 +0100

    card button theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:expenses_tracker/widgets/expenses.dart';
 
-var kColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
 void main() {
   runApp(
-     MaterialApp(
-      theme: ThemeData().copyWith(
-        useMaterial3: true,
-        colorScheme: kColorScheme,
-        appBarTheme: AppBarTheme(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer,
-        )
-
-      ),
-      home : const Expenses()
-    ),
+    MaterialApp(
+        theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: kColorScheme,
+          appBarTheme: AppBarTheme(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          ),
+          cardTheme: const CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
+            ),
+          ),
+          
+        ),
+        home: const Expenses()),
   );
 }
